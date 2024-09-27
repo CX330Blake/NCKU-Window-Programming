4b97d4e70917f6994a78ef1b5d06c03a73c3bcf8
DESKTOP-RIVTRKI\user <timchen975@gmail.com>
2024-09-27 17:32:22 +0800

    .

---
+++ HW1-2/HW1_2.cs
@@ -0,0 +1,301 @@
+﻿public class HW1_2
+{
+    static bool Exit = false;
+    static bool Login = false;
+    static int CurrentAccountIndex = 0; // Account num - 10000
+    static int[] RegisteredAccounts = new int[90000]; // 99999 - 10000 + 1
+    static int[] AccountBalances = Enumerable.Repeat(10000, 90000).ToArray();
+    // static int Balance = AccountBalances[CurrentAccountIndex];
+    static int TotalDonation = 0;
+    static int LovePoints = 0;
+    static List<int> History = new List<int>();
+
+    public static void Main(string[] args)
+    {
+        InitAccounts();
+        while (!Exit)
+        {
+            while (!Login)
+            {
+                Console.WriteLine("Please enter your account number: ");
+                int Account = Str2Int(Console.ReadLine());
+                if (Register(Account))
+                {
+                    Login = true;
+                    CurrentAccountIndex = Account - 10000;
+                }
+            }
+            Menu();
+        }
+    }
+
+    public static void Menu()
+    {
+        Console.WriteLine("\n==== MENU ====");
+        Console.WriteLine("[0] View balance");
+        Console.WriteLine("[1] Withdraw");
+        Console.WriteLine("[2] Deposit");
+        Console.WriteLine("[3] Transfer");
+        Console.WriteLine("[4] Donate");
+        Console.WriteLine("[5] Trade history");
+        Console.WriteLine("[8] Exit");
+        Console.Write("> Enter your option: ");
+
+        int option = -1;
+        try
+        {
+            option = Str2Int(Console.ReadLine());
+            Console.WriteLine("\n");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("> Invalid input. Please enter a valid number.");
+            return;
+        }
+        switch (option)
+        {
+            case 0:
+                ViewBalance();
+                break;
+            case 1:
+                Withdraw();
+                break;
+            case 2:
+                Deposit();
+                break;
+            case 3:
+                Transfer();
+                break;
+            case 4:
+                Donate();
+                break;
+            case 5:
+                TradeHistory();
+                break;
+            case 8:
+                Exit = true;
+                Console.WriteLine("> Thank you for using our service.");
+                break;
+            case 65304:
+                SecretOption();
+                break;
+            default:
+                Console.WriteLine("> Invalid option. Please enter a valid number.");
+                break;
+        }
+    }
+
+    public static void ViewBalance() { Console.WriteLine("> Your balance is " + AccountBalances[CurrentAccountIndex] + "."); }
+    public static void Withdraw()
+    {
+        History.Add(1);
+        Console.WriteLine("> Please enter the amount you want to withdraw: ");
+        int Amount = Str2Int(Console.ReadLine());
+
+        if (ValidAmount(Amount))
+        {
+            AccountBalances[CurrentAccountIndex] -= Amount;
+            Console.WriteLine("> Withdraw successful. Your new balance is: " + AccountBalances[CurrentAccountIndex]);
+            History.Add(AccountBalances[CurrentAccountIndex]);
+        }
+        else
+        {
+            return;
+        }
+
+
+    }
+    public static void Deposit()
+    {
+        History.Add(2);
+        Console.WriteLine("> Please enter the amount you want to deposit: ");
+        int Amount = Str2Int(Console.ReadLine());
+        if (ValidAmount(Amount))
+        {
+            AccountBalances[CurrentAccountIndex] += Amount;
+            Console.WriteLine("> Deposit successful. Your new balance is: " + AccountBalances[CurrentAccountIndex]);
+            History.Add(AccountBalances[CurrentAccountIndex]);
+        }
+        else
+        {
+            return;
+        }
+
+    }
+    public static void Transfer()
+    {
+        History.Add(3);
+        Console.WriteLine("> Please enter the account you want to transfer: ");
+        int Account = Str2Int(Console.ReadLine());
+        if (Account < 9999 || Account > 100000) { Console.WriteLine("> Invalid input. Please enter a valid account number."); }
+        else if (!AccountExists(Account))
+        {
+            Console.WriteLine("> Account does not exist. Do you want to register?");
+            int ConfirmRegister = Str2Int(Console.ReadLine());
+            if (ConfirmRegister == 1)
+            {
+                Register(Account);
+                AccountBalances[Account - 10000] = 0;
+            }
+        }
+        else if (Account == CurrentAccountIndex + 10000)
+        {
+            Console.WriteLine("> You cannot transfer to yourself.");
+        }
+        if (ValidAccount(Account) && AccountExists(Account))
+        {
+            int AccountIndex = Account - 10000;
+            Console.WriteLine("> Please enter the amount you want to transfer: ");
+            int Amount = Str2Int(Console.ReadLine());
+
+            if (ValidAmount(Amount))
+            {
+                int UsePoint = 0;
+                int Fee = 0;
+                if (LovePoints > 0)
+                {
+                    Console.WriteLine("> You have " + LovePoints + " love points. Do you want to use 1 point to cancel the fee?");
+                    UsePoint = Str2Int(Console.ReadLine());
+                }
+                if (UsePoint == 0)
+                {
+                    Fee = (int)Math.Floor(Amount * 0.1);
+
+                }
+                int Total = Amount + Fee;
+                Console.WriteLine("> Total (including fee): " + Total);
+                if (ValidAmount(Total))
+                {
+                    AccountBalances[CurrentAccountIndex] -= Total;
+                    AccountBalances[AccountIndex] += Amount;
+                    Console.WriteLine("> Transfer successful. Your new balance is: " + AccountBalances[CurrentAccountIndex]);
+                    History.Add(AccountBalances[CurrentAccountIndex]);
+                }
+                else
+                {
+                    Console.WriteLine("> Insufficient balance. Please deposit more.");
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+    }
+
+    public static void Donate()
+    {
+        History.Add(4);
+        Console.WriteLine("> Please enter the amount you want to donate: ");
+        int Amount = Str2Int(Console.ReadLine());
+        if (ValidAmount(Amount))
+        {
+            TotalDonation += Amount;
+            AccountBalances[CurrentAccountIndex] -= Amount;
+            Console.WriteLine("> Donation successful. Your new balance is: " + AccountBalances[CurrentAccountIndex]);
+            LovePoints += TotalDonation / 1000;
+            TotalDonation = TotalDonation % 1000;
+            Console.WriteLine("> You have " + LovePoints + " love points now.");
+            History.Add(AccountBalances[CurrentAccountIndex]);
+        }
+        else
+        {
+            return;
+        }
+    }
+    public static void TradeHistory()
+    {
+        Console.WriteLine("> Your trade history:");
+        for (int i = 0; i < History.Count; i += 2)
+        {
+            Console.WriteLine(History[i] + " - " + History[i + 1]);
+        }
+    }
+    public static bool ValidAmount(int Amount)
+    {
+        if (0 > Amount || Amount > 100000 || Amount > AccountBalances[CurrentAccountIndex])
+        {
+            Console.WriteLine("> Invalid input. Please enter a number between 0 to 100000.");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public static bool ValidAccount(int Account)
+    {
+        if (0 > Account || Account > 100000)
+        {
+            Console.WriteLine("> Invalid input. Please enter a number between 0 to 100000.");
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+    public static int Str2Int(string str)
+    {
+        try
+        {
+            return int.Parse(str);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("> Invalid input. Please enter a valid number.");
+            return -1;
+        }
+    }
+
+    public static bool AccountExists(int Account)
+    {
+        int index = Array.IndexOf(RegisteredAccounts, Account);
+        if (index == -1)
+        {
+            return false;
+        }
+        else
+        {
+            return true;
+        }
+    }
+
+
+    public static void InitAccounts()
+    {
+        for (int i = 10; i < 20; i++)
+        {
+            Register(i * 1000);
+        }
+    }
+    public static bool Register(int Account)
+    {
+        if (Account < 10000 || Account > 99999)
+        {
+            Console.WriteLine("> Invalid input. Please enter a 5 digitals number.");
+        }
+        else if (ValidAccount(Account) && !AccountExists(Account))
+        {
+            int AccountIndex = Account - 10000;
+            RegisteredAccounts[AccountIndex] = Account;
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("> Account already exists. Please enter a different account number.");
+        }
+        return false;
+    }
+
+    public static void SecretOption()
+    {
+        for (int i = 0; i < 90000; i++)
+        {
+            if (RegisteredAccounts[i] > 0)
+            {
+                Console.WriteLine(RegisteredAccounts[i] + " - " + AccountBalances[i]);
+            }
+        }
+    }
+}
---
+++ HW2-1/HW2_1.cs
@@ -0,0 +1,168 @@
+﻿public class HW2_1
+{
+    public static bool close = false;
+    public static bool alert = false;
+    public static int NumOfGoods;
+    public static string[] GoodsName; // 商品名稱陣列
+    public static int[] GoodsPrice; // 商品價格陣列
+    public static int[] GoodsRemain; // 商品庫存陣列
+    public static int[] GoodsSelled; // 商品售出陣列
+    public static int Income = 0;
+    public static void Main()
+    {
+        while (!close) { Menu(); }
+    }
+    public static void Menu()
+    {
+        Console.WriteLine("歡迎來到 NCKU 卡比商店交易系統");
+        Console.WriteLine("======================================");
+        Console.WriteLine("(1) 開店");
+        Console.WriteLine("(2) 新增訂單");
+        Console.WriteLine("(3) 查詢庫存");
+        Console.WriteLine("(4) 查詢總收入");
+        Console.WriteLine("(5) 計算人氣商品排名");
+        Console.WriteLine("(6) 關店");
+        Console.WriteLine("======================================");
+        Console.Write("請輸入您現在想要進行的操作: ");
+        int choice = int.Parse(Console.ReadLine());
+        switch (choice)
+        {
+            case 1:
+                OpenShop();
+                break;
+            case 2:
+                AddOrder();
+                break;
+            case 3:
+                CheckStock();
+                break;
+            case 4:
+                CheckIncome();
+                break;
+            case 5:
+                Rank();
+                break;
+            case 6:
+                CloseShop();
+                break;
+            default:
+                Console.WriteLine("無效的選擇，請重新輸入");
+                break;
+        }
+    }
+    public static void OpenShop()
+    {
+        Console.Write("請輸入今日總共有幾種商品要販售: ");
+        NumOfGoods = int.Parse(Console.ReadLine());
+        GoodsRemain = new int[NumOfGoods];
+        GoodsName = new string[NumOfGoods];
+        GoodsPrice = new int[NumOfGoods];
+        GoodsSelled = new int[NumOfGoods];
+        Console.Write("請依序輸入每一種商品的名稱: ");
+
+        string input = Console.ReadLine();
+        GoodsName = input.Split(' ');
+        Console.Write("接下來，請你依序輸入每一個商品的價格: ");
+        input = Console.ReadLine();
+        GoodsPrice = input.Split(' ').Select(int.Parse).ToArray();
+        Console.Write("\n輸入完成! 每一種商品的價格依序為: \n");
+        for (int i = 0; i < NumOfGoods; i++)
+        {
+            Console.WriteLine(GoodsName[i] + " : " + GoodsPrice[i]);
+        }
+        Console.Write("\n最後，請你依序輸入每一個商品目前的庫存數量: ");
+        GoodsRemain = new int[NumOfGoods];
+        input = Console.ReadLine();
+        GoodsRemain = input.Split(' ').Select(int.Parse).ToArray();
+        Console.Write("\n輸入完成! 每一種商品的庫存數量依序為: \n");
+        for (int i = 0; i < NumOfGoods; i++)
+        {
+            Console.WriteLine(GoodsName[i] + " : " + GoodsRemain[i]);
+            if (GoodsRemain[i] <= 5)
+            {
+                alert = true;
+            }
+        }
+        Console.WriteLine("\n開店程序完成，已開店\n");
+    }
+    public static void AddOrder()
+    {
+        Console.Write("請依序輸入此訂單每一種類的商品各需要買幾個: ");
+        int[] NumToBuy = new int[NumOfGoods];
+        NumToBuy = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+        int Cost = 0;
+
+        for (int i = 0; i < NumOfGoods; i++)
+        {
+            if (NumToBuy[i] > GoodsRemain[i])
+            {
+                Console.WriteLine("\n庫存不足，此筆訂單不成立\n");
+                return;
+            }
+            else
+            {
+                Cost += GoodsPrice[i] * NumToBuy[i];
+            }
+        }
+        Console.WriteLine("\n訂單成立!，總金額為: " + Cost);
+        Console.Write("請輸入消費者付款金額: ");
+
+        int Payment;
+        bool PaySuccess = false;
+
+        while (!PaySuccess)
+        {
+            Payment = int.Parse(Console.ReadLine());
+            if (Payment == -1)
+            {
+                Console.WriteLine("訂單取消\n");
+                return;
+            }
+            else if (Payment < Cost)
+            {
+                Console.Write("\n付款金額不足，請再輸入一次 (或輸入 -1 直接取消此筆訂單): ");
+            }
+            else
+            {
+                int Change = Payment - Cost;
+                Console.WriteLine("\n付款完成! 請找零消費者共 " + Change);
+                for (int i = 0; i < NumOfGoods; i++)
+                {
+                    GoodsRemain[i] -= NumToBuy[i];
+                    GoodsSelled[i] += NumToBuy[i];
+                    if (GoodsRemain[i] <= 5)
+                    {
+                        alert = true;
+                    }
+                }
+                PaySuccess = true;
+                Income += Cost;
+            }
+        }
+    }
+    public static void CheckStock()
+    {
+
+        if (alert)
+        {
+            Console.WriteLine("有商品的庫存量不足！！");
+        }
+        else
+        {
+            for (int i = 0; i < NumOfGoods; i++)
+            {
+                Console.WriteLine(GoodsName[i] + ": " + GoodsRemain[i]);
+            }
+        }
+
+    }
+    public static void CheckIncome()
+    {
+        Console.WriteLine("總收入為: " + Income);
+    }
+    public static void Rank() { }
+    public static void CloseShop()
+    {
+        close = true;
+    }
+}
